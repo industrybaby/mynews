fe72ccfa65ce48fc7d1d47ad460f28145c081f9c
RIKI FUJII <Kinshasa.2438@gmail.com>
2022-06-26 15:04:06 +0900

    4-15 ニュース投稿画面

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,16 +1,57 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
 @extends('layouts.profile')
-
-
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
 @section('title', 'プロフィールの新規作成')
 
-{{-- admin.profile.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
-    <div class="container">
+<div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィール新規作成</h2>
+                <h2>プロフィールの新規作成</h2>
+                <form action= ([ProfileController::class, 'create']); method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">                        
+                        <label class="col-md-2" for="title">性別</label>
+                            <div class="col-md-10">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                    <label class="form-check-label" for="flexRadioDefault1">男性</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                                    <label class="form-check-label" for="flexRadioDefault2">女性</label>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">趣味
+                        </label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="10">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">自己紹介                            
+                        </label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="15">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,24 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Admin\NewsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function() {
-    Route::get('news/create', 'App\Http\Controllers\Admin\NewsController@add');
-    Route::get('profile/create', 'App\Http\Controllers\Admin\ProfileController@add');
-    Route::get('profile/edit', 'App\Http\Controllers\Admin\ProfileController@edit');
-  });  
-Auth::routes();
+
+Route::controller(NewsController::class)->prefix('admin')->middleware('auth')->group(function(){
+  Route::get('news/create', 'add');
+  Route::post('news/create', 'create');
+});
+
+use App\Http\Controllers\Admin\ProfileController;
+
+Route::controller(ProfileController::class)->prefix('admin')->middleware('auth')->group(function() {
+  Route::get('profile/create', 'add');
+  Route::post('profile/create', 'create');
+  Route::get('profile/edit', 'edit');
+  Route::post('profile/edit', 'update');
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
